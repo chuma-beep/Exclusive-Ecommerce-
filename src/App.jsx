45cd8f44111aed871a80fb0c7ed374a3e3d6230b
chuma-beep <wisboynelson123@gmail.com>
2024-05-01 09:36:36 +0100

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import LandingPage from "./components/LandingPage";
-
+import SignUp from "./components/SignUp";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+       <Route path="/signup" element={<SignUp />} />
         {/* <Route path="/*" element={<NotFound />} /> */}
       </Routes>
     </Router>
