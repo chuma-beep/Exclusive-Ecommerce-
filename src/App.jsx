ace00cefd4347b273a763d660d1c43452be598d1
chuma-beep <wisboynelson123@gmail.com>
2024-05-05 17:22:22 +0100

    first cart page submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Wishlist from "./components/Wishlist";
 import JustForYou from "./components/JustForYou";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-       <Route path="/signup" element={<SignUp />} />
-       <Route path="/login" element={<Login />} />
-       <Route path="/wishlist" element={<Wishlist />} />
-       <Route path="/JustForYou" element={<JustForYou />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/JustForYou" element={<JustForYou />} />
+        <Route path="cart" element={<Cart />} />
         {/* <Route path="/*" element={<NotFound />} /> */}
       </Routes>
     </Router>
